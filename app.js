498ebe4630c0563262252adc191fad494338b2c8
Amarachi Dimkpa <amaradimkpa1@gmail.com>
2023-01-02 16:00:37 +0100

    Added some files including JavaScript file.

---
+++ app.js
@@ -0,0 +1,18 @@
+const menu = document.querySelector('.menu');
+const hamburger = document.querySelector('.btn-hamburger');
+const closeIcon = document.querySelector('.closeIcon');
+const menuIcon = document.querySelector('.menuIcon');
+
+function toggleMenu() {
+  if (menu.classList.contains('showMenu')) {
+    menu.classList.remove('showMenu');
+    closeIcon.style.display = 'none';
+    menuIcon.style.display = 'block';
+  } else {
+    menu.classList.add('showMenu');
+    closeIcon.style.display = 'block';
+    menuIcon.style.display = 'none';
+  }
+}
+
+hamburger.addEventListener('click', toggleMenu);
