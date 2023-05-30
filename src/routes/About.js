bf0d21467e2dd27c5b56b69b82e7c3740c8ec4fd
Amarachi Dimkpa <amaradimkpa1@gmail.com>
2023-05-30 22:01:03 +0100

    Reuse the HeroImg2 to connect

--- src/routes/About.js
+++ src/routes/About.js
@@ -1,7 +1,16 @@
-import React from 'react'
+import React from 'react';
+import NavBar from "../components/navBar";
+import Footer from "../components/Footer";
+import HeroImg2 from '../components/HeroImg2';
 
 const About = () => {
-  return <div className="mother">About</div>;
+  return (
+    <div>
+      <NavBar />
+      <HeroImg2 heading="About." text="I'm a friendly Full-stack Developer."  />
+      <Footer />
+    </div>
+  );
 }
 
 export default About
--- src/routes/Contact.js
+++ src/routes/Contact.js
@@ -1,7 +1,16 @@
 import React from 'react'
+import NavBar from "../components/navBar";
+import Footer from "../components/Footer";
+import HeroImg2 from '../components/HeroImg2';
 
 const Contact = () => {
-  return <div className="mother">Contact</div>;
+  return (
+    <div>
+      <NavBar />
+      <HeroImg2 heading="Contact." text="Let's have a chat." />
+      <Footer />
+    </div>
+  );
 }
 
 export default Contact
