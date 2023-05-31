c50759bdd8dc2ce159cfa28d23ce9d45947b6a67
Amarachi Dimkpa <amaradimkpa1@gmail.com>
2023-05-31 09:17:29 +0100

    Add price cards

--- src/routes/Project.js
+++ src/routes/Project.js
@@ -2,11 +2,13 @@ import React from 'react'
 import NavBar from '../components/navBar';
 import Footer from '../components/Footer';
 import HeroImg2 from '../components/HeroImg2';
+import PricingCards from '../components/PricingCards';
 
 const Project = () => {
   return <div >
     <NavBar />
     <HeroImg2 heading = "PROJECTS." text = "Some of my most recent works"/>
+    <PricingCards />
     <Footer />
   </div>;
 }
