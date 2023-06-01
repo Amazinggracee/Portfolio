ba6f902948c619db0ff4c43f2389efe2dab58e09
Amarachi Dimkpa <amaradimkpa1@gmail.com>
2023-06-01 22:19:55 +0100

    Add my Contact form

--- src/routes/Contact.js
+++ src/routes/Contact.js
@@ -2,12 +2,14 @@ import React from 'react'
 import NavBar from "../components/navBar";
 import Footer from "../components/Footer";
 import HeroImg2 from '../components/HeroImg2';
+import Form from '../components/Form';
 
 const Contact = () => {
   return (
     <div>
       <NavBar />
       <HeroImg2 heading="Contact." text="Let's have a chat." />
+      <Form />
       <Footer />
     </div>
   );
