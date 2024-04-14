27074f1e8826de94e05fecd2b6748ed906492489
Amarachi Dimkpa <amaradimkpa1@gmail.com>
2024-04-14 19:48:17 -0400

    Updated

--- src/components/HeroImg.js
+++ src/components/HeroImg.js
@@ -4,7 +4,6 @@ import { Link } from 'react-router-dom';
 import AOS from 'aos';
 import IntroImg from '../assets/heroImg.jpg';
 import 'aos/dist/aos.css';
-import './HeroImg.css';
 
 AOS.init({ duration: 3000 });
 
