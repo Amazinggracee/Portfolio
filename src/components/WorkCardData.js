cb1a4479911669b1e2a45d5c824cb048f5c415ba
AMARACHI DIMKPA <amaradimkpa1@gmail.com>
2024-08-11 18:14:53 -0400

    Update WorkCardData.js

--- src/components/WorkCardData.js
+++ src/components/WorkCardData.js
@@ -14,7 +14,7 @@ const projectCardData = [
   {
     imgsrc: Pro1,
     title: 'Personal Support Worker',
-    text: 'Kindness can transform someone’s dark moment with a blaze of light. You’ll never know how much your caring matters.' - Amy Leigh Mercree. There are only four kinds of people in the world: Those who have been caregivers, those who are currently caregivers, those who will be caregivers, and those who will need a caregiver.',
+    text: 'Kindness can transform someone’s dark moment with a blaze of light. You’ll never know how much your caring matters." - Amy Leigh Mercree. There are only four kinds of people in the world: Those who have been caregivers, those who are currently caregivers, those who will be caregivers, and those who will need a caregiver."',
     view: 'https://amazinggraceeportfolio.netlify.app/',
     source: 'https://github.com/Amazinggracee/NACC-Q-AND-A',
   },
