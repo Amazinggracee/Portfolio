2a413649e958b08a960f0e747ef85794674c675c
AMARACHI DIMKPA <amaradimkpa1@gmail.com>
2025-03-03 17:14:30 -0500

    Added an update in my about page

--- src/components/AboutContent.js
+++ src/components/AboutContent.js
@@ -13,16 +13,17 @@ const AboutContent = () => (
     <div className="left">
       <h1>Who Am I</h1>
       <p data-aos="fade-right">
-      Hello, I am Amarachi Dimkpa—a skilled React Software Developer, 
-      dedicated Social Service Worker, and compassionate Personal Support Worker. 
-      With expertise in software engineering, 
-      I build innovative solutions while also providing exceptional 
-      care and support to individuals in need. I am committed to continuous 
-      learning, staying updated with industry advancements, 
-      and excelling in dynamic environments. My strengths lie in problem-solving,
-       time management, and fostering collaboration within teams. Passionate about making a 
-       positive impact, I strive to create meaningful change in both technology and social services. 
-      Let's work together to make the world a better place!
+        Hello, I am Amarachi Dimkpa—a skilled React Software Developer,
+        dedicated Social Service Worker, and compassionate Personal Support Worker.
+        With expertise in software engineering,
+        I build innovative solutions while also providing exceptional
+        care and support to individuals in need. I am committed to continuous
+        learning, staying updated with industry advancements,
+        and excelling in dynamic environments. My strengths lie in problem-solving,
+        time management, and fostering collaboration within teams. Passionate about making a
+        positive impact, I strive to create meaningful change in both technology
+        and social services.
+        Lets work together to make the world a better place!
       </p>
       <Link to="/contact">
         <button className="btn" type="submit">
